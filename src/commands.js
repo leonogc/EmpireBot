f3d61fd4a0d35f2c89ea8cd4d155501ef4284783
ZoeiroVader <vortexgamedevs@gmail.com>
2019-06-24 20:37:37 -0300

    command switch

--- src/commands.js
+++ src/commands.js
@@ -8,4 +8,64 @@ exports.selectCommand = (message, args) =>{
         castle.help(message);
     }
     console.log(message);
+    switch (args) {
+        case 'help':
+          //
+          break;
+        case 'start':
+          //
+          break;
+        case 'stats':
+          //
+          break;
+        case 'quest':
+            //
+            break;   
+        case 'loot':
+            //
+            break;  
+        case 'loot <user>':
+            //
+            break; 
+        case 'sell <nick>':
+            //
+            break;
+        case 'trade <nick> <resource> <qtd> <resource2> <qtd2>':
+            //
+            break;
+        case 'ctrade <nick>':
+            //
+            break;
+        case 'buy <resource>':
+            //
+            break;
+        case 'market':
+            //
+            break;
+        case 'sell <resource>':
+            //
+            break;
+        case 'gsc <recurso> <preco>':
+            //
+            break;
+        case 'recruit <type> <qtd>':
+            //
+            break;
+        case 'plant <semente>':
+            //
+            break;
+        case 'expandEmpire':
+            //
+            break;
+        case 'castle':
+            //
+            break;
+        case 'castle <nick>':
+            //
+            break;
+        default:
+          //Instruções executadas quando o valor da expressão é diferente de todos os cases
+          break;
+      }
 }
+
